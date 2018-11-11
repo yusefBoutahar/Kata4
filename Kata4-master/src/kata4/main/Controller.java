960b0475dbe5f2a822e4069efb00792a8bf3d241
Yousuf Boutahar El Maachi <yusef_boutahar@hotmail.com>
2018-11-11 15:19:59 +0000

    Version 2

--- Kata4-master/src/kata4/main/Controller.java
+++ Kata4-master/src/kata4/main/Controller.java
@@ -6,14 +6,29 @@ import kata4.view.MailHistogramBuilder;
 import kata4.view.MailListReader;
 
 public class Controller {
-    public static void main(String[] args) {
-        String fileName = "src/File/emailsfile.txt";
-        
-        List<String> mailList = MailListReader.read(fileName);
-        Histogram<String> histogram = MailHistogramBuilder.build(mailList);
-        
-        HistogramDisplay histoDisplay = new HistogramDisplay(histogram);
-        histoDisplay.execute();
+	
+    private static List<String> mailList;
+    private static Histogram<String> histogram;
+    
+     public static void main(String[] args) {
+        execute();
     }
-
+     
+     private static void execute(){
+        Input();
+        Process();
+        Output();
+    }
+     
+     private static void Input(){
+        mailList = MailListReader.read("src/File/emailsfile.txt");
+    }
+     
+     private static void Process(){
+        histogram = MailHistogramBuilder.build(mailList);
+    }
+     
+     private static void Output(){
+        (new HistogramDisplay(histogram)).execute();
+    }
 }
